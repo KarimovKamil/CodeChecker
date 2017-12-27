02ccc7ae81e3e54de92e708bc47f521648d9f6c0
KarimovKamil <imanymuch@gmail.com>
2017-12-27 16:38:13 +0300

    add MockChecker realization

--- src/main/java/ru/itis/inform/checkers/MockChecker.java
+++ src/main/java/ru/itis/inform/checkers/MockChecker.java
@@ -1,12 +1,26 @@
 package ru.itis.inform.checkers;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import org.mockito.Mock;
 
 /**
  * Created by Kamil Karimov on 26.12.2017.
  */
 public class MockChecker implements Checker {
     public String start(ArrayList<Class> classes) {
-        return null;
+        for (Class aClass : classes) {
+            Field[] fields = aClass.getDeclaredFields();
+            for (Field field : fields) {
+                Annotation[] annotations = field.getAnnotations();
+                for (Annotation annotation : annotations) {
+                    if (annotation instanceof Mock) {
+                        return "В проекте используются Mock-объекты библиотеки Mockito";
+                    }
+                }
+            }
+        }
+        return "В проекте не используются Mock-объекты библиотеки Mockito";
     }
 }
--- src/main/java/ru/itis/inform/checkers/POJOChecker.java
+++ src/main/java/ru/itis/inform/checkers/POJOChecker.java
@@ -50,7 +50,7 @@ public class POJOChecker implements Checker {
 
         StringBuilder s = new StringBuilder("POJO: ");
         for (int i = 0; i < pojos.size() - 1; i++) {
-            s.append(pojos.get(i).getName() + ", ");
+            s.append(pojos.get(i).getName()).append(", ");
         }
         if (pojos.size() > 0) {
             s.append(pojos.get(pojos.size() - 1));

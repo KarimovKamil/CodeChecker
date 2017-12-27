5977479cc70aed2ba771edfe88070ef12d5b9de9
morjonan <skotchya@gmail.com>
2017-12-27 15:41:58 +0300

    add RepositoryChecker

--- src/main/java/ru/itis/inform/checkers/RepositoryChecker.java
+++ src/main/java/ru/itis/inform/checkers/RepositoryChecker.java
@@ -7,6 +7,33 @@ import java.util.ArrayList;
  */
 public class RepositoryChecker implements Checker {
     public String start(ArrayList<Class> classes) {
-        return null;
+        int crud = 0;
+        int jpa = 0;
+        for (int i = 0; i < classes.size(); i++) {
+            Class curClass = classes.get(i);
+            Class<?>[] superClasses = curClass.getInterfaces();
+            for (Class<?> superClass : superClasses) {
+                if ("org.springframework.data.jpa.repository.JpaRepository".equals(superClass.getName())) {
+                    jpa++;
+                } else {
+                    if ("org.springframework.data.repository.CrudRepository".equals(superClass.getName())) {
+                        crud++;
+                    }
+                }
+            }
+        }
+        if (jpa != 0 && crud != 0) {
+            return ("В проекте используется JpaRepository и CrudRepository");
+        } else {
+            if (crud != 0) {
+                return ("В проекте используется CrudRepository");
+            } else {
+                if (jpa != 0) {
+                    return ("В проекте используется JpaRepository");
+                } else {
+                    return ("В проекте не используется ни JpaRepository, ни CrudRepository");
+                }
+            }
+        }
     }
 }

f8eb52507769b957768ddfe423a400e84730d92d
morjonan <skotchya@gmail.com>
2017-12-28 16:57:23 +0300

    add hashCheck

--- src/main/java/ru/itis/inform/checkers/classcheckers/HashExistenceChecker.java
+++ src/main/java/ru/itis/inform/checkers/classcheckers/HashExistenceChecker.java
@@ -1,14 +1,40 @@
 package ru.itis.inform.checkers.classcheckers;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.itis.inform.checkers.Checker;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 /**
  * Created by Kamil Karimov on 26.12.2017.
  */
 public class HashExistenceChecker implements Checker {
+    boolean hash = false;
+
     public String start(ArrayList<Class> classes) {
-        return null;
+        for (Class curClass : classes) {
+            if (curClass.getName().contains("hash") || curClass.getName().contains("Hash")) {
+                hash = true;
+                break;
+            }
+            Method methods[] = curClass.getDeclaredMethods();
+            for (Method method : methods) {
+                Annotation annotations[] = method.getAnnotations();
+                for (Annotation annotation : annotations) {
+                    if (annotation instanceof Bean && method.getReturnType().equals(PasswordEncoder.class)) {
+                        hash = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (hash) {
+            return ("Есть хэширование паролей");
+        } else {
+            return ("Нет хэширования паролей");
+        }
     }
 }

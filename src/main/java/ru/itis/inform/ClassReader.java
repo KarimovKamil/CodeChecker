7a5dd489ac230f33811e0ff9ceae183ea7632f00
arturias <antegzeperob@gmail.com>
2017-12-28 19:19:48 +0300

    fix class reader

--- src/main/java/ru/itis/inform/ClassReader.java
+++ src/main/java/ru/itis/inform/ClassReader.java
@@ -5,18 +5,31 @@ package ru.itis.inform;
  */
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class ClassReader {
 
     public ArrayList<Class> getClassesInPackage(File dir) {
+        URL url = null;
+        try {
+            url = new URL("file://" + dir.toString().replace('\\', '/') + "/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(dir.toString().replace('\\', '/') + "/");
+        URL[] classUrls = { url };
+        URLClassLoader urlClassLoader = new URLClassLoader(classUrls);
+
         ArrayList<Class> classes = new ArrayList<>();
         for (File file : dir.listFiles()) {
             HashSet<String> ser = find(file);
             for (String s : ser) {
                 try {
-                    classes.add(Class.forName(s));
+                    classes.add(urlClassLoader.loadClass(s));
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -34,10 +47,13 @@ public class ClassReader {
                     setOfClasses.add(s);
                 }
             }
-        } else if (file.getName().endsWith(".java")) {
-            String[] split = file.getAbsolutePath().split("java\\\\");
-            setOfClasses.add(split[1].replace('\\', '.')
-                    .substring(0, split[1].length() - 5));
+        } else if (file.getName().endsWith(".class")) {
+            String[] split = file.getAbsolutePath().split("classes\\\\");
+            String resource = split[1].replace('\\', '.')
+                    .substring(0, split[1].length() - 6);
+            if (resource.indexOf('$') == -1) {
+                setOfClasses.add(resource);
+            }
         }
         return setOfClasses;
     }
